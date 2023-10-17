decee3c719f6bbbddf557a5c7e8356599758fb2b
nickfrosty <nfrostbutter@gmail.com>
2023-10-17 04:43:39 -0400

    refactor: memoize

--- src/components/DataCard.tsx
+++ src/components/DataCard.tsx
@@ -1,17 +1,16 @@
 "use client";
 
 import Image from "next/image";
-
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import styles from "@/styles/DataCard.module.css";
 import { numberFormatter } from "@/utils/helpers";
+import { memo } from "react";
 
 type ComponentProps = {
   treeData: TreeOptions;
   cost: number;
 };
 
-export default function DataCard({ treeData, cost }: ComponentProps) {
+export const DataCard = memo(({ treeData, cost }: ComponentProps) => {
   /**
    * NOTE: Each proof required to be included in transaction takes up 32 bytes
    * (similar to a PublicKey)
@@ -21,51 +20,51 @@ export default function DataCard({ treeData, cost }: ComponentProps) {
   const proofBytes = proofSize * 32;
 
   return (
-    <div>
-      <div className={styles.card}>
-        <h4 className={styles.cost}>
-          <Image
-            src={"/logos/solana-icon-black.svg"}
-            width={18}
-            height={18}
-            alt=""
-            blurDataURL={"/logos/solana-icon-black.svg"}
-          />
-          <span>{cost ? `~${numberFormatter(cost)}` : "---"} SOL </span>
-        </h4>
+    <div className={styles.card}>
+      <h4 className={styles.cost}>
+        <Image
+          src={"/logos/solana-icon-black.svg"}
+          width={18}
+          height={18}
+          alt=""
+          blurDataURL={"/logos/solana-icon-black.svg"}
+        />
+        <span>{cost ? `~${numberFormatter(cost)}` : "---"} SOL </span>
+      </h4>
 
-        <table className={styles.table}>
-          <tbody>
-            <tr>
-              <td className={styles.label}>
-                maxDepth
-                {/* <InformationCircleIcon  /> */}
-              </td>
-              <td className={styles.value}>{treeData.maxDepth}</td>
-            </tr>
-            <tr>
-              <td className={styles.label}>maxBufferSize</td>
-              <td className={styles.value}>{treeData.maxBufferSize}</td>
-            </tr>
-            <tr>
-              <td className={styles.label}>canopyDepth</td>
-              <td className={styles.value}>{treeData.canopyDepth}</td>
-            </tr>
-            <tr>
-              <td className={styles.label}>proof size</td>
-              <td className={styles.value}>{proofSize}</td>
-            </tr>
-            <tr>
-              <td className={styles.label}>proof bytes</td>
-              <td className={styles.value}>{proofBytes}</td>
-            </tr>
-          </tbody>
-        </table>
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <td className={styles.label}>
+              maxDepth
+              {/* <InformationCircleIcon  /> */}
+            </td>
+            <td className={styles.value}>{treeData.maxDepth}</td>
+          </tr>
+          <tr>
+            <td className={styles.label}>maxBufferSize</td>
+            <td className={styles.value}>{treeData.maxBufferSize}</td>
+          </tr>
+          <tr>
+            <td className={styles.label}>canopyDepth</td>
+            <td className={styles.value}>{treeData.canopyDepth}</td>
+          </tr>
+          <tr>
+            <td className={styles.label}>proof size</td>
+            <td className={styles.value}>{proofSize}</td>
+          </tr>
+          <tr>
+            <td className={styles.label}>proof bytes</td>
+            <td className={styles.value}>{proofBytes}</td>
+          </tr>
+        </tbody>
+      </table>
 
-        <h4 className={`text-center`}>
-          {proofSize == 3 ? "Uncompressed equivalent" : treeData.message}
-        </h4>
-      </div>
+      <h4 className={`text-center`}>
+        {proofSize == 3 ? "Uncompressed equivalent" : treeData.message}
+      </h4>
     </div>
   );
-}
+});
+
+export default DataCard;
